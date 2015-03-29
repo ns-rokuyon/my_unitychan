472f6e5f11791bbaaa048916d2d5c4102d3de991
ns-rokuyon <ness3psi@gmail.com>
2015-03-29 21:31:58 +0900

    add path viewer

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Path/TwoPointsPath.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TwoPointsPath : MonoBehaviour {
+    public Vector3[] endpoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
