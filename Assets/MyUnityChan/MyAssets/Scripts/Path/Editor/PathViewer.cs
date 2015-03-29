a6ded974269fc3e0509b32d0014b994b10101ca6
ns-rokuyon <ness3psi@gmail.com>
2015-03-29 21:56:50 +0900

    add editor

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Path/Editor/PathViewer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TwoPointsPath))]
+public class PathViewer : Editor {
+
+    private TwoPointsPath path;
+
+    void OnEnable() {
+        path = target as TwoPointsPath;
+    }
+
+    void OnSceneGUI() {
+        Vector3[] endpoints = path.endpoint;
+        Handles.color = Color.magenta;
+        Handles.DrawLine(endpoints[0], endpoints[1]);
+    }
+
+    public override void OnInspectorGUI() {
+        Vector3[] endpoints = path.GetComponent<TwoPointsPath>().endpoint;
+        path.endpoint[0].z = path.gameObject.transform.position.z;
+        path.endpoint[1].z = path.gameObject.transform.position.z;
+
+        DrawDefaultInspector();
+    }
+
+
+}
