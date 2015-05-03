d7952910da507a1d5e7811b37c0567a7422ca301
ns-rokuyon <ness3psi@gmail.com>
2015-05-03 22:40:36 +0900

    bone object caching

--- Assets/MyUnityChan/MyAssets/Scripts/Player/Action/Beam.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Player/Action/Beam.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 namespace MyUnityChan {
     public class PlayerBeam : PlayerAction {
+        private string left_shoulder_path = 
+            "Character1_Reference/Character1_Hips/Character1_Spine/Character1_Spine1/Character1_Spine2/Character1_LeftShoulder/";
+        private Transform left_arm;
+        private Transform left_fore_arm;
+        private Transform left_hand;
+        
         public PlayerBeam(Character character)
             : base(character) {
+            left_arm = player.transform.Find(left_shoulder_path + "Character1_LeftArm");
+            left_fore_arm = player.transform.Find(left_shoulder_path + "Character1_LeftArm/Character1_LeftForeArm");
+            left_hand = player.transform.Find(left_shoulder_path + "Character1_LeftArm/Character1_LeftForeArm/Character1_LeftHand");
         }
 
         public override string name() {
@@ -17,30 +26,16 @@ namespace MyUnityChan {
 
         // play beam-shot motion
         public override void performLate() {
-            string left_shoulder_path = 
-                "Character1_Reference/Character1_Hips/Character1_Spine/Character1_Spine1/Character1_Spine2/Character1_LeftShoulder/";
-            Transform bone;
-
             // hand up to front
             if ( player.isLookAhead() ) {
-                bone = player.transform.Find(left_shoulder_path + "Character1_LeftArm");
-                bone.rotation = Quaternion.AngleAxis(180.0f, new Vector3(0, 0, 1));
-
-                bone = player.transform.Find(left_shoulder_path + "Character1_LeftArm/Character1_LeftForeArm");
-                bone.localRotation = Quaternion.AngleAxis(-90.0f, new Vector3(1, 0, 0));
-
-                bone = player.transform.Find(left_shoulder_path + "Character1_LeftArm/Character1_LeftForeArm/Character1_LeftHand");
-                bone.localRotation = Quaternion.AngleAxis(-45.0f, new Vector3(0, 1, 0));
+                left_arm.rotation = Quaternion.AngleAxis(180.0f, new Vector3(0, 0, 1));
+                left_fore_arm.localRotation = Quaternion.AngleAxis(-90.0f, new Vector3(1, 0, 0));
+                left_hand.localRotation = Quaternion.AngleAxis(-45.0f, new Vector3(0, 1, 0));
             }
             else {
-                bone = player.transform.Find(left_shoulder_path + "Character1_LeftArm");
-                bone.rotation = Quaternion.AngleAxis(0.0f, new Vector3(0, 0, 1));
-
-                bone = player.transform.Find(left_shoulder_path + "Character1_LeftArm/Character1_LeftForeArm");
-                bone.localRotation = Quaternion.AngleAxis(90.0f, new Vector3(1, 0, 0));
-
-                bone = player.transform.Find(left_shoulder_path + "Character1_LeftArm/Character1_LeftForeArm/Character1_LeftHand");
-                bone.localRotation = Quaternion.AngleAxis(-45.0f, new Vector3(0, 1, 0));
+                left_arm.rotation = Quaternion.AngleAxis(0.0f, new Vector3(0, 0, 1));
+                left_fore_arm.localRotation = Quaternion.AngleAxis(90.0f, new Vector3(1, 0, 0));
+                left_hand.localRotation = Quaternion.AngleAxis(-45.0f, new Vector3(0, 1, 0));
             }
         }
 
