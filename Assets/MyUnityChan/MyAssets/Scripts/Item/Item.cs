f33a3a2c84cc1b180f84ada61d6730b26e1c38b5
ns-rokuyon <ness3psi@gmail.com>
2015-05-24 21:05:46 +0900

    add Item Base class

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Item/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public abstract class Item : PoolObjectBase {
+
+        public abstract void perform(Player player);
+
+        public void OnCollisionStay(Collision collisionInfo) {
+            if ( collisionInfo.gameObject.tag == "Player" ) {
+                Player player = collisionInfo.gameObject.GetComponent<Player>();
+                perform(player);
+            }
+        }
+    }
+}
