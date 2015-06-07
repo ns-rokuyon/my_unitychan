afac94cb5d6a4f79fd002561c1a068dc2aa14a01
ns-rokuyon <ness3psi@gmail.com>
2015-06-07 22:34:09 +0900

    add object rotation

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Behavior/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public class Spin : ObjectBase {
+        public Vector3 angle;
+
+        // Update is called once per frame
+        void Update() {
+            transform.Rotate(angle * Time.deltaTime);
+        }
+    }
+}
