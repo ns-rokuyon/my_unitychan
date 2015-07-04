96a32b5109bb5a3bdcc68628fd87128a6885a8ad
ns-rokuyon <ness3psi@gmail.com>
2015-07-04 23:54:51 +0900

    add ability item base

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Item/KeyItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public abstract class KeyItem : ObjectBase {
+        public abstract void perform(Player player);
+
+        public abstract void destroy(Player player);
+
+        public void setPosition(Vector3 pos) {
+            gameObject.transform.position = pos;
+        }
+
+        public void OnCollisionStay(Collision collisionInfo) {
+            if ( collisionInfo.gameObject.tag == "Player" ) {
+                Player player = collisionInfo.gameObject.GetComponent<Player>();
+                perform(player);
+                destroy(player);
+            }
+        }
+
+        public void OnTriggerStay(Collider colliderInfo) {
+            if ( colliderInfo.gameObject.tag == "Player" ) {
+                Player player = colliderInfo.gameObject.GetComponent<Player>();
+                perform(player);
+                destroy(player);
+            }
+        }
+    }
+}
