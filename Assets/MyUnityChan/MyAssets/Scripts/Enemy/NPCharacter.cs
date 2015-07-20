d1713f6b7978fe067416ea9ca448c2e736dcffa5
ns-rokuyon <ness3psi@gmail.com>
2015-07-20 20:26:11 +0900

    split source files for AttackSpec and NPCharacter class

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Enemy/NPCharacter.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+namespace MyUnityChan {
+    public class NPCharacter : Character {
+        static protected List<GameObject> players = new List<GameObject>();
+        const int PLAYER_TOUCHING_FRAME_OFFSET = 10;
+
+        static public void setPlayers() {
+            removeNullPlayers();
+            foreach ( GameObject pl in GameObject.FindGameObjectsWithTag("Player") ) {
+                players.Add(pl);
+            }
+        }
+
+        static public void removeNullPlayers() {
+            players.RemoveAll(pl => pl == null);
+        }
+
+        static public GameObject findPlayerByName(string name) {
+            foreach ( GameObject player in players ) {
+                Player script = player.GetComponent<Player>();
+                if ( script.player_name == name ) {
+                    return player;
+                }
+            }
+            throw new System.InvalidOperationException("not exists : " + name);
+        }
+
+        static public GameObject findNearestPlayer(Vector3 pos) {
+            if ( players.Count == 1 ) {
+                return players[0];
+            }
+
+            GameObject nearest = null;
+            float min_dist = 100000.0f;
+            foreach ( GameObject player in players ) {
+                if ( nearest == null ) {
+                    nearest = player;
+                    continue;
+                }
+
+                float tmp_dist = Vector3.Distance(nearest.transform.position, player.transform.position);
+                if ( tmp_dist < min_dist ) {
+                    min_dist = tmp_dist;
+                    nearest = player;
+                }
+            }
+            return nearest;
+        }
+
+        protected Dictionary<string, int> touching_players = new Dictionary<string, int>();
+
+        protected virtual void start() { }
+        protected virtual void update() { }
+        protected virtual void die() { }
+
+        protected void checkPlayerTouched() {
+            foreach ( KeyValuePair<string, int> pair in touching_players ) {
+                if ( pair.Value > PLAYER_TOUCHING_FRAME_OFFSET ) {
+                    GameObject player = findPlayerByName(pair.Key);
+                    Player script = player.GetComponent<Player>();
+
+                    script.damage();
+                }
+            }
+        }
+
+        protected void faceForward() {
+            float dir_x = controller.keyHorizontal();
+            gameObject.transform.LookAt(new Vector3(gameObject.transform.position.x + dir_x * 100.0f, gameObject.transform.position.y, transform.position.z));
+        }
+
+        public void OnCollisionStay(Collision collisionInfo) {
+            if ( collisionInfo.gameObject.tag == "Player" ) {
+                Player player_script = collisionInfo.gameObject.GetComponent<Player>();
+                string player_name = player_script.player_name;
+
+                if ( touching_players.ContainsKey(player_name) ) {
+                    touching_players[player_name]++;
+                }
+                else {
+                    touching_players[player_name] = 1;
+                }
+            }
+        }
+
+        public void OnCollisionExit(Collision collisionInfo) {
+            if ( collisionInfo.gameObject.tag == "Player" ) {
+                Player player_script = collisionInfo.gameObject.GetComponent<Player>();
+                touching_players[player_script.player_name] = 0;
+            }
+        }
+    }
+}
---
+++ Assets/MyUnityChan/MyAssets/Scripts/Player/AttackSpec.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public abstract class AttackSpec {
+        public int damage { get; set; }
+        public int stun { get; set; }       // time enemy is stuned
+        public int frame { get; set; }      // time hitbox enabled
+        public string effect_name { get; set; }
+        public abstract void attack(Character character, Hitbox hitbox);
+    }
+}
