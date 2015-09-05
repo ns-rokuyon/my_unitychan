07ecc9803c2a05d7909eff92eeedb88237c028a8
ns-rokuyon <ness3psi@gmail.com>
2015-09-05 22:48:13 +0900

    add removeComponent method extension to GameObject

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Extension/Component.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public static class Component {
+        public static GameObject removeComponent<T>(this GameObject self) where T : UnityEngine.Component {
+            GameObject.Destroy(self.GetComponent<T>());
+            return self;
+        }
+
+    }
+}
