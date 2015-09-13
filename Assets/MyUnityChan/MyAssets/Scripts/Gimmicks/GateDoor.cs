9d23078613221b02ee1cc7c0bc00fd0290b47fe9
ns-rokuyon <ness3psi@gmail.com>
2015-09-13 16:48:51 +0900

    change door open/close methods

--- Assets/MyUnityChan/MyAssets/Scripts/Gimmicks/GateDoor.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Gimmicks/GateDoor.cs
@@ -4,13 +4,13 @@ using System.Collections;
 namespace MyUnityChan {
     public class GateDoor : Door {
         public override void open() {
-            transform.localScale = new Vector3(0.0f, transform.localScale.y, transform.localScale.z);
             GetComponent<Collider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
         }
 
         public override void close() {
-            transform.localScale = new Vector3(2.2f, transform.localScale.y, transform.localScale.z);
             GetComponent<Collider>().enabled = true;
+            GetComponent<MeshRenderer>().enabled = true;
         }
     }
 }
