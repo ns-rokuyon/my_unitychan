c632956a386d401fefbd633659b54d204d01a186
ns-rokuyon <ness3psi@gmail.com>
2015-09-24 23:54:25 +0900

    add menu base class

---
+++ Assets/MyUnityChan/MyAssets/Scripts/UI/Menu/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace MyUnityChan {
+    public class MenuButton : GUIObjectBase, ISelectHandler {
+
+        public void OnSelect(BaseEventData event_data) {
+            iTween.ShakeRotation(this.gameObject, iTween.Hash("z", 10, "time", 0.4f));
+        }
+    }
+}
---
+++ Assets/MyUnityChan/MyAssets/Scripts/UI/Menu/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyUnityChan {
+    public class MenuManager : SingletonObjectBase<MenuManager> {
+        public GameObject canvas_object;
+
+        private EventSystem es;
+        private List<Selectable> selectables;
+
+        void Awake() {
+            es = EventSystem.current;
+
+            selectables = new List<Selectable>();
+            foreach ( Selectable selectable in canvas_object.GetComponentsInChildren<Selectable>() ) {
+                selectables.Add(selectable);
+            }
+        }
+
+        void Update() {
+        }
+    }
+}
