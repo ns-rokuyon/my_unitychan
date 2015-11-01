d48674b75da1c529040ffc03438d1b684f18d273
ns-rokuyon <ness3psi@gmail.com>
2015-11-01 22:49:25 +0900

    add vector3 extension

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Extension/Vector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Vector {
+
+    public static Vector3 flipX(this Vector3 vec) {
+        return new Vector3(-vec.x, vec.y, vec.z);
+    }
+
+}
