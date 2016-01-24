182339fa66a375698806957e345415d7861dd64f
ns-rokuyon <ness3psi@gmail.com>
2016-01-24 21:57:47 +0900

    add util class

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Behavior/RealDeltaTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public class RealDeltaTime : ObjectBase {
+        public float delta_time { get; set; }
+        private float last_delta_time = 0.0f;
+
+        void Start() {
+            last_delta_time = Time.realtimeSinceStartup;
+        }
+
+        void Update() {
+            delta_time = Time.realtimeSinceStartup - last_delta_time;
+            last_delta_time = Time.realtimeSinceStartup;
+        }
+    }
+}
--- Assets/MyUnityChan/MyAssets/Scripts/Util/Behavior/Spin.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Behavior/Spin.cs
@@ -4,9 +4,19 @@ using System.Collections;
 namespace MyUnityChan {
     public class Spin : ObjectBase {
         public Vector3 angle;
+        private RealDeltaTime pause_canceller;
+
+        void Awake() {
+            pause_canceller = GetComponent<RealDeltaTime>();
+        }
 
         // Update is called once per frame
         void Update() {
+            if ( PauseManager.isPausing() && pause_canceller ) {
+                transform.Rotate(angle * pause_canceller.delta_time);
+                return;
+            }
+
             transform.Rotate(angle * Time.deltaTime);
         }
     }
