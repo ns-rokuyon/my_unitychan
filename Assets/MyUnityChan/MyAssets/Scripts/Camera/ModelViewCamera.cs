b8b6d1688fb916b5c820c1587ee57b51f4e16789
ns-rokuyon <ness3psi@gmail.com>
2016-01-24 21:59:58 +0900

    add camera for render texture

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Camera/ModelViewCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyUnityChan {
+    public class ModelViewCamera : ObjectBase {
+        public GameObject target;
+        public float distance;
+        public Vector3 angle;
+        public Vector3 offset;
+
+        void Update() {
+            if ( !target ) return;
+
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z - distance);
+            transform.position = transform.position.add(offset.x, offset.y, offset.z);
+            gameObject.transform.rotation = Quaternion.Euler(angle);
+        }
+    }
+}
