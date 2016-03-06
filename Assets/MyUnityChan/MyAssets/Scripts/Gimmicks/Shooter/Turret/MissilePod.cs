c49cdc22542b798e0b2df7cbfed6df759c05469a
ns-rokuyon <ness3psi@gmail.com>
2016-03-06 17:24:38 +0900

    add missile num

--- Assets/MyUnityChan/MyAssets/Scripts/Gimmicks/Shooter/Turret/MissilePod.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Gimmicks/Shooter/Turret/MissilePod.cs
@@ -1,16 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 namespace MyUnityChan {
     public class MissilePod : TurretBase {
         public string missile_name;
+        public int missile_max;
+        public GameObject indicator_ui_object;
+
+        public int missile_num { get; protected set; }
+
+        protected Text indicator = null;
+
+        void Awake() {
+            indicator = indicator_ui_object.GetComponent<Text>();
+        }
 
         void Start() {
             baseStart();
             setProjectile(missile_name);
+            missile_num = missile_max;
+        }
+
+        void Update() {
+            baseUpdate();
+            if ( indicator ) indicator.text = "" + missile_num;
         }
 
         public override void shoot() {
+            Debug.Log(missile_num);
+            if ( missile_num == 0 ) {
+                return;
+            }
+            missile_num--;
+
             GameObject obj = ObjectPoolManager.getGameObject(Const.Prefab.Projectile[missile_name]);
             obj.setParent(Hierarchy.Layout.PROJECTILE);
 
@@ -28,6 +51,12 @@ namespace MyUnityChan {
             sound();
         }
 
+        public void addMissile(int n) {
+            missile_num += n;
+            if ( missile_num > missile_max ) {
+                missile_num = missile_max;
+            }
+        }
 
     }
 }
