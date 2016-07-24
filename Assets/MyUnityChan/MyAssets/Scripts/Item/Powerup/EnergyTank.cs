332c108feab0e0fc1084059f81e34a0c595b6d5e
ns-rokuyon <ness3psi@gmail.com>
2016-07-24 22:19:11 +0900

    implement energy item

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Item/Powerup/EnergyTank.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public class EnergyTank : PowerupItem {
+        private static GameText name = GameText.text("Energy tank");
+        private static GameText description = GameText.text("Max HP +100");
+
+        public override void destroy(Player player) {
+            ModalManager.Instance.show(name, description);
+            PauseManager.Instance.pause(true, ModalManager.Instance.control, () => { ModalManager.Instance.hide(); });
+            Destroy(this.gameObject);
+        }
+
+        public override void perform(Player player) {
+            player.manager.status.addEnergyTank();
+        }
+
+    }
+
+}
