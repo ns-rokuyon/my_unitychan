045cdecb6cf280f12ffac41375f34aaba3ebd1ac
ns-rokuyon <ness3psi@gmail.com>
2017-01-22 21:41:52 +0900

    add swing

--- Assets/MyUnityChan/MyAssets/Scripts/Gimmicks/GrapplingHook.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Gimmicks/GrapplingHook.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using UniRx;
 
 namespace MyUnityChan {
     [RequireComponent(typeof(LineRenderer))]
@@ -21,6 +23,7 @@ namespace MyUnityChan {
         public LineRenderer line { get; set; }
         public Player player { get; set; }
         public SpringJoint joint { get; set; }
+        public IDisposable position_updater { get; set; }
 
         void Awake() {
             line = GetComponent<LineRenderer>();
@@ -34,19 +37,44 @@ namespace MyUnityChan {
                 line.SetPosition(1, hand.transform.position);
         }
 
+        public void initPosition(RaycastHit hit) {
+            var obj = hit.collider.gameObject;
+            var character = obj.GetComponent<Character>();
+            if ( character ) {
+                position_updater = Observable.EveryUpdate()
+                    .Subscribe(_ => hook.transform.position = obj.transform.position);
+            }
+            else {
+                hook.transform.position = hit.point;
+                position_updater = null;
+            }
+        }
+
         public void connect(Player _player) {
             player = _player;
             hand.transform.position = player.transform.position;
             hand.AddComponent<SpringJoint>();
             joint = hand.GetComponent<SpringJoint>();
             joint.connectedBody = player.rigid_body;
-            joint.spring = 1500;
-            joint.damper = 2000;
+            joint.spring = 5000;
+            joint.damper = 0;
         }
 
         public void disconnect() {
+            if ( position_updater != null ) {
+                position_updater.Dispose();
+                position_updater = null;
+            }
+
             joint.connectedBody = null;
             Destroy(transform.parent.gameObject);
+        }
+
+        public float getSwingDegree() {
+            Vector3 basepoint = hook.transform.position;
+            Vector3 handpoint = line.GetPosition(1);
+            return Mathf.Atan2(Mathf.Abs(basepoint.x - handpoint.x),
+                               Mathf.Abs(basepoint.y - handpoint.y)) * Mathf.Rad2Deg;
         }
     }
 }
--- Assets/MyUnityChan/MyAssets/Scripts/Player/Action/Grapple.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Player/Action/Grapple.cs
@@ -7,7 +7,9 @@ namespace MyUnityChan {
         public float max_distance { get; set; }
         public float radius { get; set; }
         public bool grappled { get; set; }
-        public GameObject hook { get; set; }
+
+        public GameObject hook { get; set; }                // GameObject
+        public GrapplingHook grapplinghook { get; set; }    // Component
 
         private RaycastHit ghit;
 
@@ -26,6 +28,9 @@ namespace MyUnityChan {
             (match to LocalPosition.y of Hand object in GrapplingHook prefab)
             */
             max_distance = 4.0f;
+
+            // Register child action
+            player.action_manager.registerAction(new PlayerSwing(ch));
         }
 
         public bool raycast() {
@@ -38,9 +43,10 @@ namespace MyUnityChan {
             bool success = raycast();
             if ( !grappled && success ) {
                 hook = PrefabInstantiater.create(Const.Prefab.Gimmick[Const.ID.Gimmick.GRAPPLING_HOOK]);
-                hook.transform.position = ghit.point;
-                hook.GetComponentInChildren<GrapplingHook>().length = max_distance;
-                hook.GetComponentInChildren<GrapplingHook>().connect(player);
+                grapplinghook = hook.GetComponentInChildren<GrapplingHook>();
+                grapplinghook.initPosition(ghit);
+                grapplinghook.length = max_distance;
+                grapplinghook.connect(player);
                 grappled = true;
             }
         }
@@ -55,7 +61,7 @@ namespace MyUnityChan {
                 right_hand.localRotation = Quaternion.AngleAxis(45.0f, new Vector3(0, 1, 0));
             }
             else {
-                right_arm.rotation = Quaternion.AngleAxis(0.0f, new Vector3(0, 0, 1));
+                right_arm.rotation = Quaternion.AngleAxis(290.0f, new Vector3(0, 0, 1));
                 right_fore_arm.localRotation = Quaternion.AngleAxis(90.0f, new Vector3(1, 0, 0));
                 right_hand.localRotation = Quaternion.AngleAxis(-45.0f, new Vector3(0, 1, 0));
             }
@@ -63,6 +69,7 @@ namespace MyUnityChan {
 
         public override void init() {
             player.action_manager.getAction("JUMP").perform_callbacks.Add(disconnect);
+
             right_arm = player.bone_manager.bone(Const.ID.UnityChanBone.RIGHT_ARM).transform;
             right_fore_arm = player.bone_manager.bone(Const.ID.UnityChanBone.RIGHT_FORE_ARM).transform;
             right_hand = player.bone_manager.bone(Const.ID.UnityChanBone.RIGHT_HAND).transform;
@@ -72,6 +79,7 @@ namespace MyUnityChan {
             if ( hook ) {
                 hook.GetComponentInChildren<GrapplingHook>().disconnect();
                 hook = null;
+                grapplinghook = null;
                 grappled = false;
             }
         }
@@ -86,6 +94,54 @@ namespace MyUnityChan {
 
         public override string name() {
             return "GRAPPLE";
+        }
+    }
+
+    public class PlayerSwing : PlayerAction {
+        public float swing_deg_limit;   // Add swing force to player when hook degree is in this range
+
+        private PlayerGrapple grapple;
+        private Rigidbody rb;
+        private Vector3 swingF;
+
+        public PlayerSwing(Character ch) : base(ch) {
+            grapple = null;
+            rb = null;
+            swingF = new Vector3(2000.0f, 0, 0);
+            swing_deg_limit = 20.0f;
+        }
+
+        public override void init() {
+            grapple = player.action_manager.getAction<PlayerGrapple>("GRAPPLE");
+            rb = player.GetComponent<Rigidbody>();
+        }
+
+        public override void performFixed() {
+            if ( rb == null )
+                return;
+            if ( grapple.grapplinghook == null )
+                return;
+            if ( grapple.grapplinghook.getSwingDegree() > swing_deg_limit )
+                return;
+
+            var horizontal = controller.keyHorizontal();
+            rb.AddForce(swingF * horizontal);
+        }
+
+        public override bool condition() {
+            if ( grapple == null ) {
+                init();
+                return false;
+            }
+            return grapple.grappled && controller.keyHorizontal() != 0.0f;
+        }
+
+        public override Const.PlayerAction id() {
+            return Const.PlayerAction.SWING;
+        }
+
+        public override string name() {
+            return "SWING";
         }
     }
 }
