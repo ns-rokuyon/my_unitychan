67702bc616dcafa23709239c829a1c32cd6225bd
ns-rokuyon <ness3psi@gmail.com>
2017-03-11 22:34:39 +0900

    add animation extension

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Extension/Animation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+namespace MyUnityChan {
+    public static class AnimationExtension {
+        public static void PlayAndNext(this Animation self, string anim1, string anim2) {
+            self.Play(anim1);
+            Observable.Timer(System.TimeSpan.FromSeconds(self.GetClip(anim1).length))
+                .Subscribe(_ => {
+                    self.Blend(anim2);
+                });
+        }
+    }
+}
