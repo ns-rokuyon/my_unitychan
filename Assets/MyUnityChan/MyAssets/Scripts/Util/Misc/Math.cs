2ab8249d1ac9477988e5fa1103ed01d4b015a084
ns-rokuyon <ness3psi@gmail.com>
2017-08-14 23:54:45 +0900

    add misc class

---
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Misc/Math.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyUnityChan {
+    public partial class Misc {
+        public static Vector3 degree2DirectionVector3(float degree) {
+            return new Vector3(Mathf.Cos(degree), Mathf.Sin(degree), 0.0f);
+        }
+    }
+}
