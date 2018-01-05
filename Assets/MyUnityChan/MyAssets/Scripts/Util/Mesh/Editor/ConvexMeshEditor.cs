0fd4f5ba219906179a42d2b85276e9184f676233
ns-rokuyon <ness3psi@gmail.com>
2018-01-05 23:53:35 +0900

    multiple area objects support

--- Assets/MyUnityChan/MyAssets/Scripts/Util/Mesh/Editor/ConvexMeshEditor.cs
+++ Assets/MyUnityChan/MyAssets/Scripts/Util/Mesh/Editor/ConvexMeshEditor.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyUnityChan {
 #if UNITY_EDITOR
@@ -11,7 +13,12 @@ namespace MyUnityChan {
             ConvexMesh obj = target as ConvexMesh;
             base.OnInspectorGUI();
 
-            if ( GUILayout.Button("Make") ) {
+            if ( GUILayout.Button("Reset mesh") ) {
+                obj.GetComponent<MeshFilter>().sharedMesh.Clear();
+                obj.GetComponent<MeshCollider>().sharedMesh.Clear();
+            }
+
+            if ( GUILayout.Button("Make a convex mesh with target_objects") ) {
                 if ( !obj.target_object ) {
                     Debug.LogWarning("No target object");
                     return;
@@ -24,84 +31,35 @@ namespace MyUnityChan {
                 }
 
                 Mesh mesh = ConvexHull.createMesh(target_meshfilter.sharedMesh.vertices);
-                obj.GetComponent<MeshFilter>().mesh = mesh;
+                obj.GetComponent<MeshFilter>().sharedMesh = mesh;
                 obj.GetComponent<MeshCollider>().sharedMesh = mesh;
             }
 
-            if ( GUILayout.Button("Reset mesh") ) {
-                obj.GetComponent<MeshFilter>().mesh.Clear();
-                obj.GetComponent<MeshCollider>().sharedMesh.Clear();
-            }
-        }
-    }
-
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(MeshFilter))]
-    [CustomEditor(typeof(Area), true)]
-    class AreaConvexMeshEditor : Editor {
-        public override void OnInspectorGUI() {
-            Area area = target as Area;
-            base.OnInspectorGUI();
-
-            if ( GUILayout.Button("Make a convex mesh") ) {
+            if ( GUILayout.Button("Make a convex mesh with Area.area_objects") ) {
+                Area area = obj.GetComponent<Area>();
+                if ( !area ) {
+                    Debug.LogWarning("No Area components");
+                    return;
+                }
                 if ( area.area_objects.Count == 0 ) {
-                    Debug.LogWarning("No target object");
+                    Debug.LogWarning("No area objects");
                     return;
                 }
 
-                area.gameObject.transform.localScale = area.area_objects[0].transform.localScale;
-                area.gameObject.transform.localRotation = area.area_objects[0].transform.localRotation;
-                area.gameObject.transform.position = area.area_objects[0].transform.position;
-
-                // TODO: support multiple area_objects
-                Mesh mesh = ConvexHull.createMesh(area.area_objects[0].GetComponent<MeshFilter>().sharedMesh.vertices);
-                area.GetComponent<MeshFilter>().sharedMesh = mesh;
-
-                if ( area.GetComponent<BoxCollider>() ) {
-                    area.gameObject.removeComponent<BoxCollider>();
-                }
-                if ( area.GetComponent<MeshCollider>() ) {
-                    area.gameObject.removeComponent<MeshCollider>();
-                }
-
-                // Add MeshCollider
-                var meshcollider = area.gameObject.AddComponent<MeshCollider>();
-                meshcollider.sharedMesh = mesh;
-                meshcollider.convex = true;
-                meshcollider.isTrigger = true;
-            }
-
-            if ( GUILayout.Button("Reset mesh") ) {
-                // Create and attach cube mesh 
-                area.GetComponent<MeshFilter>().sharedMesh.Clear();
-                area.GetComponent<MeshFilter>().sharedMesh = 
-                    GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<MeshFilter>().sharedMesh;
-
-                if ( area.GetComponent<MeshCollider>() ) {
-                    area.gameObject.removeComponent<MeshCollider>();
-                }
-
-                // Add BoxCollider
-                var boxcollider = area.gameObject.AddComponent<BoxCollider>();
-                boxcollider.isTrigger = true;
+                List<Vector3> vertices = new List<Vector3>();
+                area.area_objects.ForEach(area_object => {
+                    MeshFilter mf = area_object.GetComponent<MeshFilter>();
+                    if ( mf ) {
+                        vertices.AddRange(mf.sharedMesh.vertices.Select(v => v + area_object.transform.localPosition));
+                        Debug.Log("Vertices of [" + area_object.name + "] were added (" +
+                                  mf.sharedMesh.vertices.Length + ")");
+                    }
+                });
+                Mesh mesh = ConvexHull.createMesh(vertices);
+                obj.GetComponent<MeshFilter>().sharedMesh = mesh;
+                obj.GetComponent<MeshCollider>().sharedMesh = mesh;
             }
         }
     }
-
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(MeshFilter))]
-    [CustomEditor(typeof(NormalArea))]
-    class NormalAreaConvexMeshEditor : AreaConvexMeshEditor { }
-
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(MeshFilter))]
-    [CustomEditor(typeof(BossArea))]
-    class BossAreaConvexMeshEditor : AreaConvexMeshEditor { }
-
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(MeshFilter))]
-    [CustomEditor(typeof(SafeArea))]
-    class SafeAreaConvexMeshEditor : AreaConvexMeshEditor { }
 #endif
-
 }
